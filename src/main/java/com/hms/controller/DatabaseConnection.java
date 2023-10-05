659388f6d88a789885a064e622ce9cd8dfb17865
arif amiruddin <76756035+psakurai@users.noreply.github.com>
2023-10-05 14:50:42 +0800

    Add public key retrieval

--- src/main/java/com/hms/controller/DatabaseConnection.java
+++ src/main/java/com/hms/controller/DatabaseConnection.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/hmsdb?useSSL=false", "root", "password");
+        return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hmsdb?allowPublicKeyRetrieval=true&useSSL=false", "root", "password");
+        //return DriverManager.getConnection("jdbc:mysql://192.168.127.115:3306/hmsdb?useSSL=false", "root", "password");
     }
 }
