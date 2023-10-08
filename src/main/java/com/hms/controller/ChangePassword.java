673f9f7adc8cf64057612a95f518183d7f0df068
izzaharsyd <59030163+izzaharsyd@users.noreply.github.com>
2023-10-08 10:41:37 +0800

    sql query updated

--- src/main/java/com/hms/controller/ChangePassword.java
+++ src/main/java/com/hms/controller/ChangePassword.java
@@ -50,27 +50,34 @@ public class ChangePassword extends HttpServlet {
 		String newpassword = request.getParameter("newpassword").trim();
 		String renewpassword = request.getParameter("renewpassword").trim();
 		HttpSession session = request.getSession();
-
+		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DatabaseConnection.getConnection();
-			PreparedStatement pst = con.prepareStatement(
-					"select password from user WHERE ic = '" + session.getAttribute("USERic").toString() + "'");
-			ResultSet rs = pst.executeQuery();
-			if (rs.next()) {
-				if (password.equals(rs.getString("password"))) {
-					if (newpassword.equals(renewpassword)) {
-						Statement stat = con.createStatement();
-
-						stat.executeUpdate("update user SET password ='" + renewpassword + "'WHERE ic = '"
-								+ session.getAttribute("USERic").toString() + "'");
-
-					}
-				}
-			}
-			response.sendRedirect("jsp/Profile.jsp");
+		    Class.forName("com.mysql.jdbc.Driver");
+		    Connection con = DatabaseConnection.getConnection();
+		    
+		    // Prepare a parameterized query to retrieve the password
+		    String selectPasswordQuery = "SELECT password FROM user WHERE ic = ?";
+		    PreparedStatement selectPasswordStmt = con.prepareStatement(selectPasswordQuery);
+		    selectPasswordStmt.setString(1, session.getAttribute("USERic").toString());
+		    
+		    ResultSet rs = selectPasswordStmt.executeQuery();
+		    
+		    if (rs.next()) {
+		        String storedPassword = rs.getString("password");
+		        if (password.equals(storedPassword) && newpassword.equals(renewpassword)) {
+		            // Prepare a parameterized query to update the password
+		            String updatePasswordQuery = "UPDATE user SET password = ? WHERE ic = ?";
+		            PreparedStatement updatePasswordStmt = con.prepareStatement(updatePasswordQuery);
+		            updatePasswordStmt.setString(1, renewpassword);
+		            updatePasswordStmt.setString(2, session.getAttribute("USERic").toString());
+		            
+		            updatePasswordStmt.executeUpdate();
+		        }
+		    }
+		    
+		    response.sendRedirect("jsp/Profile.jsp");
 		} catch (Exception ex) {
-			ex.printStackTrace();
+		    ex.printStackTrace();
 		}
 
 	}
